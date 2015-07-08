ef958adda67d62c7f12b82b3a875f7973f621fff
nicolas2lee <nicolas2lee@gmail.com>
2015-07-08 16:04:50 +0200

    add test_AdvancedMYR.txt which records all the test

--- MYR_rails/app/models/member.rb
+++ MYR_rails/app/models/member.rb
@@ -10,7 +10,7 @@ class Member < ActiveRecord::Base
     validates :name, presence: true, uniqueness: true, length: { in: 3..30, too_long: "%{count} characters is the maximum allowed", too_short:"%{count} characters is the minimum allowed"  }
     has_secure_password
     validates :password, presence: true, confirmation: true, length: { in: 6..30, too_long: "%{count} characters is the maximum allowed", too_short:"%{count} characters is the minimum allowed"  }
-    validates :email, presence: true, uniqueness: true, length: { in: 5..300, too_long: "%{count} characters is the maximum allowed", too_short:"%{count} characters is the minimum allowed"  }, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'must be a email address ex: exemple@ofE.mail'}
+    validates :email, presence: true, uniqueness: true, length: { in: 7..250, too_long: "%{count} characters is the maximum allowed", too_short:"%{count} characters is the minimum allowed"  }, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'must be a email address ex: exemple@ofE.mail'}
     validates :role, inclusion: { in: %w(administrator visitor player), message: "%{value} is not a valid role" }, allow_nil: true
     validates :logo, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image'}
     validate :valid_size

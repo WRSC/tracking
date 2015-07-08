6951957778be8b85ddb120eff15379fc92f45bf8
nicolas2lee <nicolas2lee@gmail.com>
2015-07-08 15:35:18 +0200

    test static_page controller correctly

--- MYR_rails/test/controllers/static_pages_controller_test.rb
+++ MYR_rails/test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-=begin
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact"
   end
-=end
+
 
 end

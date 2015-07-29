ff8432fcbd7b1c0aa8379aa4dc6d90480dd6dc26
nicolas2lee <nicolas2lee@gmail.com>
2015-07-29 09:41:02 +0200

    fix permission problem need to create the directory before, other users do not have the permission

--- MYR_rails/app/uploaders/teamlogo_uploader.rb
+++ MYR_rails/app/uploaders/teamlogo_uploader.rb
@@ -13,7 +13,8 @@ class TeamlogoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/public/uploads/teams/teamlogo/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+   # "/public/uploads/teams/teamlogo/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/public/uploads/teams/teamlogo"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:

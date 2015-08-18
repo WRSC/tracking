72ef58a958781a434604580c4ba72ca5af1efa2b
nicolas2lee <nicolas2lee@gmail.com>
2015-08-18 13:47:01 +0200

    change some views

--- MYR_rails/app/assets/javascripts/team.js
+++ MYR_rails/app/assets/javascripts/team.js
@@ -19,22 +19,27 @@ teaminfo[1] => info about missions => [mission,[attempts]]
     		data.addColumn('string', 'ToolTip');
 //============ add robot node ===============
 				data.addRows([
-					[{v:robname, f:robname+'<div style="color:red; font-style:italic">President</div>'}, '', 'Your Robot']
+//					[{v:robname, f:robname+'<div style="color:red; font-style:italic">Robot</div>'}, '', 'Your Robot']
+					[robname, '', 'Your Robot']
 				]);
 //============ add mission node =================
 
 				for (var i=0;i<teaminfo[1].length;i++){
 // teaminfo[i] = [mission,[attempts]]
+					//alert('i:'+i)
 					m=teaminfo[1][i][0]
 					data.addRows([
-						[{v:m.name, f:m.name+'<div style="color:red; font-style:italic">Vice President</div>'}, robname, 'Mission '+m.id],
+						[{v:m.name, f:m.name+'<div style="color:Crimson; font-style:italic; width: 100%; "><font size="2">'+'Start: '+m.start+'<br />End: '+m.end+'</font></div>'}, robname, 'Mission '+m.id]
 					]);
 					atts=teaminfo[1][i][1]
 					for (var j=0; j< atts.length;j++){
-							data.addRows([
-								[atts[j].name, m.name, 'Attempt'],
-								['Tracker '+atts[j].tracker_id, atts[j].name, 'Tracker']
-					]);	
+					
+						data.addRows([
+							//[atts[j].name, m.name, 'Attempt'],
+							[{v:atts[j].name, f:atts[j].name+'<div style="color:red; font-style:italic; width: 250px"><font size="2">'+'Start: '+atts[j].start+'<br />End: '+atts[j].end+'</font></div>'}, m.name, 'Attempt'],
+							['Tracker '+atts[j].tracker_id+' => '+atts[j].name, atts[j].name, 'Tracker'],
+							
+						]);	
 					}
 				}
 				var option={

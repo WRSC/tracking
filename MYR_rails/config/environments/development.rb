c840850ff83995fffb71efe3a421eb146482e1ca
Sylvain Hunault <sylvain.hunault@ensta-bretagne.org>
2015-08-27 14:07:54 +0300

    commit#25 Sylvain
    fix link of mail to validate registration

--- MYR_rails/config/environments/development.rb
+++ MYR_rails/config/environments/development.rb
@@ -45,12 +45,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
-#		address:              'smtp.gmail.com',
-#    port:                 587,
-#    user_name:            'enstabmons@gmail.com',
-#    password:             'enstabmons2015',
-#    authentication:       'plain',
-#    enable_starttls_auto: true 
 	 address:               "smtp.gmail.com",
 	 port:                  587,
 	 user_name:             "wrscroot@gmail.com",
@@ -58,5 +52,12 @@ Rails.application.configure do
 	 authentication:        "plain",
 	 enable_starttls_auto:  true
 	}
+
+  #   address:              'smtp.gmail.com',
+#    port:                 587,
+#    user_name:            'enstabmons@gmail.com',
+#    password:             'enstabmons2015',
+#    authentication:       'plain',
+#    enable_starttls_auto: true 
 
 end

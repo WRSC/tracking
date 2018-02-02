1ed72e3076b7a270845eb069ab88f3ab613c5b72
Thomas Kluyver <thomas@kluyver.me.uk>
2018-02-02 17:02:57 +0000

    Fix test for page title

--- MYR_rails/test/controllers/static_pages_controller_test.rb
+++ MYR_rails/test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "WRSC 2015 Tracking"
+    assert_select "title", "WRSC Tracking"
   end
 
   test "should get contact" do

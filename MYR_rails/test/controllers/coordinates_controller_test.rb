5df5b1532b0f8ea9a5fd50f82006adbb03011c6b
Kir Shatrov <shatrov@me.com>
2018-04-29 17:38:25 +0100

    Cover CoordinatesController#create with tests

--- MYR_rails/test/controllers/coordinates_controller_test.rb
+++ MYR_rails/test/controllers/coordinates_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class CoordinatesControllerTest < ActionController::TestCase
-=begin
   setup do
-    @coordinate = coordinates(:one)
+    @tracker = trackers(:mytracker)
+    @coordinate = coordinates(:c1)
   end
 
   test "should get index" do
@@ -17,18 +17,45 @@ class CoordinatesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create coordinate" do
-    assert_difference('Coordinate.count') do
-      post :create, coordinate: { datetime: @coordinate.datetime, latitude: @coordinate.latitude, longitude: @coordinate.longitude, tracker_id: @coordinate.tracker_id }
+  test "creates single coordinate" do
+    assert_difference('Coordinate.count', 1) do
+      post :create, coordinate: {
+        datetime: 20150605010101,
+        latitude: "0.5",
+        longitude: "1",
+        speed: 1,
+        course: 1,
+        tracker_id: @tracker.id,
+        token: @tracker.token
+      }, format: :json
     end
 
-    assert_redirected_to coordinate_path(assigns(:coordinate))
+    assert_response :created
   end
+
+  test "creates multiple coordinates" do
+    assert_difference('Coordinate.count', 2) do
+      post :create, coordinate: {
+        datetime: "20150605010101_20150606010101",
+        latitude: "0.5_0.7",
+        longitude: "1_2",
+        speed: "1_2",
+        course: "1_1",
+        tracker_id: @tracker.id,
+        token: @tracker.token
+      }, format: :json
+    end
+
+    assert_response :created
+  end
+
 
   test "should show coordinate" do
     get :show, id: @coordinate
     assert_response :success
   end
+
+=begin
 
   test "should get edit" do
     get :edit, id: @coordinate

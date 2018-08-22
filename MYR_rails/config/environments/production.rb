5daf44fa92eaac868b1f44dac32418a62475e55f
Yu Cao <tsaoyu@tsaoyu.com>
2018-08-22 16:34:28 +0100

    New email account for activation

--- MYR_rails/config/environments/production.rb
+++ MYR_rails/config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # SMTP settings for gmail
-    config.action_mailer.default_url_options = { :host => "tracking.wrsc2017.com" }
+    config.action_mailer.default_url_options = { :host => "www.roboticsailing.org/2018/tracking" }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     address:               "smtp.gmail.com",
     port:                  587,
-    user_name:             "wrscroot@gmail.com",
-    password:              "wrscAdmin2015",
+    user_name:             "wrsc.noreply@gmail.com",
+    password:              "sailrobot",
     authentication:        "plain",
     enable_starttls_auto:  true
   }

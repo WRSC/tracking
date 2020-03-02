c0df9afedfcd46d222c06b265f0a8f695d9f6ade
Thomas Kluyver <thomas@kluyver.me.uk>
2020-03-02 13:15:01 +0000

    Update to rails 5.1

--- MYR_rails/config/application.rb
+++ MYR_rails/config/application.rb
@@ -1,6 +1,4 @@
-require File.expand_path('../boot', __FILE__)
-
-require 'csv'
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -10,22 +8,12 @@ Bundler.require(*Rails.groups)
 
 module MYRRails
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.force_ssl = false
-    config.active_record.raise_in_transactional_callbacks = true
-    config.exceptions_app = self.routes                                      #coment out Geir
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end

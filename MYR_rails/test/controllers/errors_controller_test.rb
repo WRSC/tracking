a150d2e44c87d7706f1feb432711f7d1e7f78edd
Thomas Kluyver <thomas@kluyver.me.uk>
2020-03-02 13:18:17 +0000

    Fix errors controller tests

--- MYR_rails/test/controllers/errors_controller_test.rb
+++ MYR_rails/test/controllers/errors_controller_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class ErrorsControllerTest < ActionController::TestCase
   test "should get file_not_found" do
     get :file_not_found
-    assert_response :success
+    assert_response :not_found
   end
 
   test "should get unprocessable" do
     get :unprocessable
-    assert_response :success
+    assert_response 422
   end
 
   test "should get internal_server_error" do
     get :internal_server_error
-    assert_response :success
+    assert_response 500
   end
 
 end
